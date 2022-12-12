cbe0983bb8c76da581416009897814607f06d65c
Ivio Feng <115487784+ivios@users.noreply.github.com>
2022-12-12 18:08:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+int main() {
+
+    char listaNomi[10][20];
+    start(listaNomi,10);
+    char mioNome[1][20];
+    start(mioNome,1);
+
+    for (int i = 0; i < 10; i++) {
+        cin >> listaNomi[i];
+    }
+    cin >> mioNome[0];
+
+    int pos = 0;
+    if(cercaNome(listaNomi, mioNome, pos)){
+        cout << pos;
+    } else {
+        cout << "non presente";
+    }
+
+    return 0;
 }
