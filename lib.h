3dfd406c5c5dad353c32dfb1d554093cad5a861a
Ivio Feng <115487784+ivios@users.noreply.github.com>
2022-12-12 18:09:21 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,5 @@
+bool cercaNome (char listaNomi[][20], char mioNome[][20], int &i);
 
+bool comparaRiga (char listaNomi[][20], char mioNome[][20], int &i);
+
+void start (char listaNomi[][20], int m);
